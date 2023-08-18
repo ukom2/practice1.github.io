c74d94229d6a4a19eb5d349f71c47c0c7ae6e2b1
ukom2 <141610241+ukom2@users.noreply.github.com>
2023-08-18 14:31:19 +0400

    summary blog

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/practice1.github.io/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230818.112958';
+const cacheName = 'chirpy-20230818.143055';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
